0ef5a26b797cbe2bec712af652d44b8c042fc3a8
Krishna Rawat <160142827+Krishnarawatethic@users.noreply.github.com>
2024-07-13 20:54:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
 		PADDLE: '#e64980',
 		PINBALL: '#dee2e6'
 	};
-	const GRAVITY = 0.75;
+	const GRAVITY = 0.55;
 	const WIREFRAMES = false;
 	const BUMPER_BOUNCE = 1.5;
 	const PADDLE_PULL = 0.002;
